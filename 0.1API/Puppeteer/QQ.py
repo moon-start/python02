712828c7d771d29221101f549207acbc9e2dff85
moon-start <login0516mp4@gmail.com>
2020-03-24 23:33:14 +0800

    All File

--- 0.1API/Puppeteer/QQ.py
+++ 0.1API/Puppeteer/QQ.py
@@ -1,20 +1,19 @@
-# # import asyncio
-# # from pyppeteer import launch
-# # from lxml import etree
+# pip install asyncio
+import asyncio
+from pyppeteer import launch
+from lxml import etree
 
-# # import
-# # from app18H import main
+from appHH import main
 
-# # url ="https://18h.animezilla.com/manga/3931/4"
-
-
-
-# import time
-# for  n in range(1,101):
-#     print(n)
-#     # url ="https://comicbus.live/online/comic-103.html?ch=1-"+str(i)
-#     # asyncio.get_event_loop().run_until_complete(main( url ))
+# url ="https://18h.animezilla.com/manga/3931/4"
 
 
 
-print("123")
+import time
+for  n in range(1,11):
+    print(n)
+    url ="https://comicbus.live/online/comic-103.html?ch=1-"+str(n)
+    asyncio.get_event_loop().run_until_complete(main( url,str(n) )   )
+
+
+
--- 0.1API/Puppeteer/appHH.py
+++ 0.1API/Puppeteer/appHH.py
@@ -1,3 +1,4 @@
+
 import asyncio
 from pyppeteer import launch
 from lxml import etree
@@ -33,12 +34,13 @@ pngPath = "0.1API/example.png"
 
 # url ="https://18h.animezilla.com/manga/3931/4"
 url = "https://comicbus.live/online/comic-103.html?ch=1-3"
+n = "1"
 
 
 # global ww,hh,uu
 
 
-async def main(url):
+async def main(url,n):
     print(url)
 
     global RR,ww,hh
@@ -100,31 +102,31 @@ async def main(url):
     ## 導向
     # await page.goto( doc('body img#comic').attr("src") )  
     # await page.goto( doc('body img[id=comic]').attr("src") )        ## 屬性選擇
-    await page.goto( doc('body a > p > img[id=comic]').attr("src") )  ## 指定選擇
+    # await page.goto( doc('body a > p > img[id=comic]').attr("src") )  ## 指定選擇
     
 
-    ## 本文 --------page 記錄著目前的頁面
-    doc = pq( await page.content() )
-    # print( doc('body > img').attr("width")  )
-    # print( doc('body > img').attr("height")  )
+    # ## 本文 --------page 記錄著目前的頁面
+    # doc = pq( await page.content() )
+    # # print( doc('body > img').attr("width")  )
+    # # print( doc('body > img').attr("height")  )
 
-    ww,hh,uu = doc('body > img').attr("width"),doc('body > img').attr("height"),doc('body > img').attr("src")
-    print(ww ," ",hh," ",uu)
-    xx,yy = doc('body > img').attr("Left"),doc('body > img').attr("Top")
-    print(xx ," ",yy)
+    # ww,hh,uu = doc('body > img').attr("width"),doc('body > img').attr("height"),doc('body > img').attr("src")
+    # print(ww ," ",hh," ",uu)
+    # xx,yy = doc('body > img').attr("Left"),doc('body > img').attr("Top")
+    # print(xx ," ",yy)
 
 
 
     # await browser.close()     ##  關閉
 
-    print(uu)
-    RR=uu
+    # print(uu)
+    # RR=uu
     # print(RR)
 
 
     # ## 截圖  
-    Path = "0.1API/Puppeteer/"
-    await page.screenshot({'path': Path + "example.png" })    
+    Path = "0.1API/Puppeteer/PNG/"
+    await page.screenshot({'path': Path + "example-"+n+".png" })    
     ##################################################
     # r=requests.get( RR )
     # with open(Path+'AA.jpg','wb') as f:
@@ -149,10 +151,10 @@ async def main(url):
     # await asyncio.sleep(100)  ##  不關閉
     await browser.close()     ##  關閉
 
-print("第一次")
-asyncio.get_event_loop().run_until_complete(main( url ))
-print("結束")
-print(RR," ",ww," ",hh)
+# print("第一次")
+# asyncio.get_event_loop().run_until_complete(main( url,n ))
+# print("結束")
+# print(RR," ",ww," ",hh)
 ###########################################################
 ###########################################################
 ###########################################################
@@ -162,55 +164,55 @@ print(RR," ",ww," ",hh)
 
 
 
-async def mainB():
-    print(type(ww))
-    wwa,hha = int(ww),int(hh)
-    # ww=int(ww)
-    # hh=int(hh)
-    ## chrome 的 版面
-    browser = await launch(headless=False, args=[f'--window-size={wwa},{hha}'] )  ## 這一行是 打開一個頁面
-    ## html 的 版面   
-    page = await browser.newPage()
-    await page.setViewport({'width': wwa, 'height': hha}) 
-    ######################################################
-    ##
-    ## 頁面
-    await page.goto( RR )
-    ##
-    ## html
-    html = pq(  await page.content() )
-    # html("body > img").attr("width" ,str(wwa))
-    # html("body > img").attr("height",str(hha))
-    # html("body > img").attr("width" ,"500")
-    # html("body > img").attr("height","500")
+# async def mainB():
+#     print(type(ww))
+#     wwa,hha = int(ww),int(hh)
+#     # ww=int(ww)
+#     # hh=int(hh)
+#     ## chrome 的 版面
+#     browser = await launch(headless=False, args=[f'--window-size={wwa},{hha}'] )  ## 這一行是 打開一個頁面
+#     ## html 的 版面   
+#     page = await browser.newPage()
+#     await page.setViewport({'width': wwa, 'height': hha}) 
+#     ######################################################
+#     ##
+#     ## 頁面
+#     await page.goto( RR )
+#     ##
+#     ## html
+#     html = pq(  await page.content() )
+#     # html("body > img").attr("width" ,str(wwa))
+#     # html("body > img").attr("height",str(hha))
+#     # html("body > img").attr("width" ,"500")
+#     # html("body > img").attr("height","500")
 
 
-    ##
-    ## 屬性修改
-    ## 屬性操作
-# ## .attr()  修改
-# p = pq('<p id="hello" class="helloB"></p>')('p')
-# print( p.attr("id") )              ## 傳回內容
-# print( p.attr("class") )           ## 傳回內容
-# print( p.attr("id", "plop") )
-# print( p.attr("id", "hello") )
+#     ##
+#     ## 屬性修改
+#     ## 屬性操作
+# # ## .attr()  修改
+# # p = pq('<p id="hello" class="helloB"></p>')('p')
+# # print( p.attr("id") )              ## 傳回內容
+# # print( p.attr("class") )           ## 傳回內容
+# # print( p.attr("id", "plop") )
+# # print( p.attr("id", "hello") )
 
 
 
-    dimensions = await page.evaluate('''() => {
-        var img= document.getElementsByTagName('imd')[0];
-        //var html= document.getElementsByClassName("QQ")[0];
-        //var html= document.getElementsById("QQ");
+#     dimensions = await page.evaluate('''() => {
+#         var img= document.getElementsByTagName('imd')[0];
+#         //var html= document.getElementsByClassName("QQ")[0];
+#         //var html= document.getElementsById("QQ");
         
-        //img.style.width  = 500+'px';
-        //img.style.height = 500+'px';
+#         //img.style.width  = 500+'px';
+#         //img.style.height = 500+'px';
 
-    }''')  # 执行js代码
-    # print(dimensions)  # 返回js执行结果
-    ## 將頁面設定寬高 ## 將 body設定 寬高
-    # await page.setViewport( dimensions )
+#     }''')  # 执行js代码
+#     # print(dimensions)  # 返回js执行结果
+#     ## 將頁面設定寬高 ## 將 body設定 寬高
+#     # await page.setViewport( dimensions )
 
 
-    await asyncio.sleep(100)  ##  不關閉
+#     await asyncio.sleep(100)  ##  不關閉
 
-# asyncio.get_event_loop().run_until_complete(mainB())
+# # asyncio.get_event_loop().run_until_complete(mainB())
